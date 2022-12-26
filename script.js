d066804f4557188fa2576d5f2b287b3c06f03e48
LuisCBeltran <lcarlosbeltran@gmail.com>
2022-12-26 00:05:43 -0400

    game is working now

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 const randomNumber = function (min, max) {
-    const rock = '<i class="fa-solid fa-hand-fist"></i>'
+    const rock = '<i class="fa-solid fa-hand-fist"></i>';
+    const paper = '<i class="fa-solid fa-hand"></i>';
+    const scissors = '<i class="fa-solid fa-hand-scissors"></i>';
     random = Math.floor(Math.random() * (max - min + 1)) + min;
     if (random === 1) {
         document.getElementById('cpupickimg').innerHTML = rock;
     } else if (random === 2) {
-        console.log("I pick PAPER!")
+        document.getElementById('cpupickimg').innerHTML = paper;
     } else if (random === 3) {
-        console.log("I pick SCISSORS!")
+        document.getElementById('cpupickimg').innerHTML = scissors;
+    }
+    return(random)
+}
+
+const match = function (player, cpu) {
+    const win = '<p class="resulttext">YOU WIN!</p>';
+    const lose = '<p class="resulttext">YOU LOSE!</p>';
+    const tie = '<p class="resulttext">TIE!</p>';
+    if (player === cpu) {
+        document.getElementById('resulttext').innerHTML = tie;
+    } else if (player === 1 && cpu === 2) {
+        document.getElementById('resulttext').innerHTML = lose;
+    } else if (player === 1 && cpu === 3) {
+        document.getElementById('resulttext').innerHTML = win;
+    } else if (player === 2 && cpu === 1) {
+        document.getElementById('resulttext').innerHTML = win;
+    } else if (player === 2 && cpu === 3) {
+        document.getElementById('resulttext').innerHTML = lose;
+    } else if (player === 3 && cpu === 1) {
+        document.getElementById('resulttext').innerHTML = lose;
+    } else if (player === 3 && cpu === 2) {
+        document.getElementById('resulttext').innerHTML = win;
     }
 }
 
 const rock = function (num) {
-    randomNumber(1, 3); 
+    const random = randomNumber(1, 3); 
+    match(num, random);
 }
 
 const paper = function (num) {
-    console.log("paper")
+    const random = randomNumber(1, 3); 
+    match(num, random);
 }
 
 const scissors = function (num) {
-    console.log(randomNumber(1, 3))
+    const random = randomNumber(1, 3); 
+    match(num, random);
 }
 
