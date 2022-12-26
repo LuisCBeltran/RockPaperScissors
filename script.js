8e06c52811d21a374cbad5f29b0c29d555d5841a
LuisCBeltran <lcarlosbeltran@gmail.com>
2022-12-26 10:32:30 -0400

    score system added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let cpuScore = 0
+
 const randomNumber = function (min, max) {
     const rock = '<i class="fa-solid fa-hand-fist"></i>';
     const paper = '<i class="fa-solid fa-hand"></i>';
@@ -21,31 +24,45 @@ const match = function (player, cpu) {
         document.getElementById('resulttext').innerHTML = tie;
     } else if (player === 1 && cpu === 2) {
         document.getElementById('resulttext').innerHTML = lose;
+        cpuScore++
     } else if (player === 1 && cpu === 3) {
         document.getElementById('resulttext').innerHTML = win;
+        playerScore++
     } else if (player === 2 && cpu === 1) {
         document.getElementById('resulttext').innerHTML = win;
+        playerScore++
     } else if (player === 2 && cpu === 3) {
         document.getElementById('resulttext').innerHTML = lose;
+        cpuScore++
     } else if (player === 3 && cpu === 1) {
         document.getElementById('resulttext').innerHTML = lose;
+        cpuScore++
     } else if (player === 3 && cpu === 2) {
         document.getElementById('resulttext').innerHTML = win;
+        playerScore++
     }
+}
+
+const printScore = function (playerScr, cpuScr) {
+    let scoretext = "YOU: " + playerScr + " - CPU: " + cpuScr;
+    document.getElementById('score').innerHTML = scoretext;
 }
 
 const rock = function (num) {
     const random = randomNumber(1, 3); 
     match(num, random);
+    printScore(playerScore, cpuScore);
 }
 
 const paper = function (num) {
     const random = randomNumber(1, 3); 
     match(num, random);
+    printScore(playerScore, cpuScore);
 }
 
 const scissors = function (num) {
     const random = randomNumber(1, 3); 
     match(num, random);
+    printScore(playerScore, cpuScore);
 }
 
